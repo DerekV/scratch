3185109a181ac74f6a9c5e8905c015e34e6537d3
Derek VerLee <derekverlee@gmail.com>
2014-09-15 13:02:34 -0400

    Modify steps so that they may be async

--- src/main/java/example/derekverlee/scratch/Scratch.java
+++ src/main/java/example/derekverlee/scratch/Scratch.java
@@ -1,7 +1,9 @@
 package example.derekverlee.scratch;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 public class Scratch {
     public static <Outcome> RecipeBuilderStarter<Outcome> createRecipe(Class<Outcome> clazz) {
@@ -10,9 +12,11 @@ public class Scratch {
 
 
     public static interface Recipe<Outcome> {
-        Outcome cook();
+        Outcome cook() throws InterruptedException;
 
     }
+
+
 
     public static class RecipeBuilder<FinalT, OutT> {
         private final List<Step> steps = new ArrayList<>();
@@ -27,19 +31,33 @@ public class Scratch {
             steps.add(lastStep);
             return new Recipe<FinalT>() {
                 @Override
-                public FinalT cook() {
-                    Object current = null; // start with null, first step should expect it
-                    for(Step step : steps) {
-                        current = step.apply(current);
+                public FinalT cook() throws InterruptedException {
+                    final List<FinalT> finalOutcomeBox = new ArrayList<>(1);
+                    final Iterator<Step> stepIterator = steps.iterator();
+                    final CountDownLatch latch = new CountDownLatch(1);
+                    RecipeCallback<FinalT> finalCb = new RecipeCallback<FinalT>() {
+                        @Override
+                        public void onSuccess(FinalT outcome) {
+                            finalOutcomeBox.add(outcome);
+                            latch.countDown();
+                        }
+                    };
+                    if(stepIterator.hasNext()) {
+                        Step firstStep = stepIterator.next();
+                        firstStep.apply(null, new StepResultRecurser(stepIterator, finalCb));
                     }
-                    return (FinalT) current;
+
+                    latch.await();
+                    FinalT finalOutcome = finalOutcomeBox.get(0);
+                    return finalOutcome;
                 }
+
             };
         }
     }
 
     public static interface Step<Input, Output> {
-        Output apply(Input out);
+        void apply(Input in, StepResultPasser<Output> passResult);
     }
 
     public static class RecipeBuilderStarter<FinalOutcome> {
@@ -48,4 +66,32 @@ public class Scratch {
         }
     }
 
+    public interface StepResultPasser<ResultT> {
+        void pass(ResultT in);
+    }
+
+    private static class StepResultRecurser implements StepResultPasser<Object> {
+
+        private final Iterator<Step> stepIterator;
+        private final RecipeCallback finalCb;
+
+        public StepResultRecurser(Iterator<Step> stepIterator, RecipeCallback finalCb) {
+            this.stepIterator = stepIterator;
+            this.finalCb = finalCb;
+        }
+
+        @Override
+        public void pass(Object in) {
+            if(stepIterator.hasNext()) {
+                Step step = stepIterator.next();
+                step.apply(in, new StepResultRecurser(stepIterator, finalCb));
+            } else {
+                finalCb.onSuccess(in);
+            }
+        }
+    }
+
+    public static interface RecipeCallback<FinalT> {
+        void onSuccess(FinalT outcome);
+    }
 }
--- src/test/java/example/derekverlee/scratch/ScratchTest.java
+++ src/test/java/example/derekverlee/scratch/ScratchTest.java
@@ -10,51 +10,43 @@ public class ScratchTest {
 
     public static final Scratch.Step<Void, Integer> JUST_7 = new Scratch.Step<Void, Integer>() {
         @Override
-        public Integer apply(Void in) {
-            return 7;
+        public void apply(Void in, Scratch.StepResultPasser<Integer> passResult) {
+            passResult.pass(7);
         }
     };
     public static final Scratch.Step<Integer, Integer> ADD_4 = new Scratch.Step<Integer, Integer>() {
         @Override
-        public Integer apply(Integer in) {
-            return in + 4;
+        public void apply(Integer in, Scratch.StepResultPasser<Integer> passResult) {
+            passResult.pass(in + 4);
         }
     };
     public static final PassThrough<Integer> PASS_THROUGH_INT = new PassThrough<>();
+    public static final PassThrough<Void> PASS_THROUGH_NULL = new PassThrough<Void>();
 
 
     @Test
-    public void cooking_with_nil_step() {
-        Scratch.Step<Void, Void> doNothing = new Scratch.Step<Void, Void>() {
-
-            @Override
-            public Void apply(Void out) {
-                // do nothing
-                return null;
-            }
-        };
-
-        createRecipe(Void.class).startWith(doNothing).lastly(doNothing).cook();
+    public void cooking_with_nil_step() throws Exception {
+        createRecipe(Void.class).startWith(PASS_THROUGH_NULL).lastly(PASS_THROUGH_NULL).cook();
     }
 
     @Test
-    public void cooking_up_a_number() {
+    public void cooking_up_a_number() throws Exception {
         Scratch.Recipe<Integer> recipeFor7 = createRecipe(Integer.class).startWith(JUST_7).lastly(PASS_THROUGH_INT);
         assertThat(recipeFor7.cook(), is(7));
     }
 
     @Test
-    public void adding_up_some_numbers() {
+    public void adding_up_some_numbers() throws Exception {
         Scratch.Recipe<Integer> recipeFor7 = createRecipe(Integer.class).startWith(JUST_7).lastly(ADD_4);
         assertThat(recipeFor7.cook(), is(11));
     }
 
     @Test
-    public void change_up_the_types() {
+    public void change_up_the_types() throws Exception {
         Scratch.Step<Integer, String> intToString = new Scratch.Step<Integer, String>() {
             @Override
-            public String apply(Integer in) {
-                return Integer.toString(in);
+            public void apply(Integer in, Scratch.StepResultPasser<String> result) {
+                result.pass(Integer.toString(in));
             }
         };
 
@@ -67,10 +59,49 @@ public class ScratchTest {
     }
 
 
+    @Test
+    public void async_step() throws Exception {
+        Scratch.Step<Integer, String> intToString = new Scratch.Step<Integer, String>() {
+            @Override
+            public void apply(Integer in, Scratch.StepResultPasser<String> result) {
+                result.pass(Integer.toString(in));
+            }
+        };
+
+        Scratch.Step<String, String> asyncAddWord = new Scratch.Step<String, String>() {
+            @Override
+            public void apply(final String in, final Scratch.StepResultPasser cb ) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        simulateBackgroudWork();
+                        cb.pass(in + " Dwarves");
+                    }
+                }).start();
+            }
+        };
+
+        Scratch.Recipe<String> recipeFor7 =
+                createRecipe(String.class)
+                        .startWith(JUST_7)
+                        .step(intToString)
+                        .lastly(asyncAddWord);
+        assertThat(recipeFor7.cook(), is("7 Dwarves"));
+    }
+
+    private void simulateBackgroudWork() {
+        try {
+            Thread.sleep(700);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
     private static class PassThrough<T> implements Scratch.Step<T,T> {
         @Override
-        public T apply(T in) {
-            return in;
+        public void apply(T in, Scratch.StepResultPasser<T> result) {
+            result.pass(in);
         }
     };
 }
